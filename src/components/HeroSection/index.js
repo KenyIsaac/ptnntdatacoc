6b018036ac5e73fc742e6fab4da5f806eb51b9ff
Keny Isaac <keny.isaac.lm@hotmail.com>
2022-06-16 12:38:05 -0500

    [add] added button elements

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/clasbg.mp4';
-import { HeroContainer, HeroBg, VideoBg, } from './HeroElements';
+import {Button} from '../ButtonElement';
+import { 
+    HeroContainer, 
+    HeroBg, 
+    VideoBg, 
+    HeroContent, 
+    HeroH1, 
+    HeroP, 
+    HeroBtnWrapper, 
+    ArrowForward, 
+    ArrowRight
+ } from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    };
+
+
   return (
     <HeroContainer>
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </HeroBg>
+        <HeroContent>
+            <HeroH1>Clash of Clans Page</HeroH1>
+            <HeroP>
+                Inicie sesión para comenzar.
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button 
+                    to='/signup' 
+                    onMouseEnter={onHover} 
+                    onMouseLeave={onHover}
+                    primary="true"
+                    dark="true"
+                >
+                    Inicia aquí{ hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
