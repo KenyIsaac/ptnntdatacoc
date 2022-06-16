076b5457e8069687f3c72d4ea7318b41352aec6d
Keny Isaac <keny.isaac.lm@hotmail.com>
2022-06-16 15:55:47 -0500

    [add] added infosection and images

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Conoce nuestro juego',
+    headLine: 'Clash of Clans es el juego para ti',
+    description: 'Clash of Clans, también conocido como CoC, es un videojuego de estrategia y de construcción de aldeas en línea, para dispositivos móviles con plataformas de iOS y Android. Fue desarrollado por Supercell y lanzado para iOS el 2 de agosto del 2012 y el 30 de septiembre para Android.',
+    buttonLabel: 'Descárgalo aquí',
+    imgStart: false,
+    img: require('../../images/coc01.jpg'),
+    alt: 'coc',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Nuestra funcionalidad',
+    headLine: 'Compite contra otros jugadores y conviértete en el mejor',
+    description: 'Clash of Clans es un videojuego de estrategia multijugador en el que el usuario debe construir una aldea (en una cuadrícula de 44x44), donde tendrá que administrar recursos, construir y mejorar una amplia variedad de edificios, entrenar tropas, elaborar hechizos, y atacar a otras aldeas de jugadores en línea o participar en una campaña de un jugador en la que luchará contra aldeas prediseñadas, con la finalidad de obtener tres tipos de recursos: oro, elixir y elixir oscuro; útiles para la expansión de la aldea. Al mismo tiempo, el usuario puede crear o unirse a un clan, donde podrá interactuar con otros jugadores y participar en eventos como las "Guerras de Clanes", los "Juegos Clan" o las "Ligas de Guerra".',
+    buttonLabel: 'Leer más...',
+    imgStart: true,
+    img: require('../../images/coc02.jpg'),
+    alt: 'coc',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Estructuras ideales',
+    headLine: 'Crea tu aldea y arma tu estrategia',
+    description: 'Los edificios hacen que la aldea crezca pero construirlos y mejorarlos requiere oro, elixir o elixir oscuro. Algunos pueden costar gemas, como lo es la choza de constructor. El Ayuntamiento es el principal edificio, y de su mejora dependerá la construcción de los demás edificios, una vez alcanzado el máximo de mejoras de las estructuras del nivel del Ayuntamiento, es necesario aumentar su nivel para así poder mejorar más.',
+    buttonLabel: 'Leer más...',
+    imgStart: false,
+    img: require('../../images/coc03.jpg'),
+    alt: 'coc',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
